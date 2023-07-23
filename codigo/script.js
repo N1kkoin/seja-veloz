4bb25111eaa18e62c4bd5b762d082bd60cd6cc8c
N1kkoin <nikkonh@hotmail.com>
2023-07-22 22:15:50 -0300

    seila mais mano

--- codigo/script.js
+++ codigo/script.js
@@ -1,5 +1,5 @@
 
-const wordsFile = "http://N1kkoin.github.io/seja-veloz/codigo/words.json"; // Substitua pelo caminho correto
+const wordsFile = "/codigo/words.json"; // Substitua pelo caminho correto
 let words = []; // Array que armazenará as palavras
 let correctWords = []; // Array que armazenará as palavras corretas
 
